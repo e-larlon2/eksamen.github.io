e595b5893cf8f9d08b9967be042351c6342f7ae4
Lars.github.io <e-larlon2@iskule.no>
2023-06-05 23:28:54 +0200

    commit design change

--- js/logout.js
+++ js/logout.js
@@ -1,3 +1,17 @@
+// HER LEGGER DU INN INFOEN DIN FRÅ FIREBASE
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDS2iif-fpdVI4ObThfsMcKAWflN9Fzej0",
+    authDomain: "eksamen-b35cd.firebaseapp.com",
+    projectId: "eksamen-b35cd",
+    storageBucket: "eksamen-b35cd.appspot.com",
+    messagingSenderId: "839174035802",
+    appId: "1:839174035802:web:0615567b60cece6a9e3b63"
+});
+///////////////////////////////////////////////////////////
+
+/* Firebase config */
+const auth = firebaseApp.auth();
 
 function logout() {
     auth.signOut().then(() => {
